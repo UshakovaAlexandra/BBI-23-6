daee47cb594f83ec3a89042345dc391664ed5a5a
UshakovaAlexandra <146060562+UshakovaAlexandra@users.noreply.github.com>
2023-12-24 22:43:47 +0300

    Защита 5 лабы

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-
 // 7
 using System;
- 
+
 class Program
 {
     static void Main(string[] args)
@@ -59,31 +58,39 @@ class Program
             }
             Console.WriteLine();
         }
- 
+
         // Поиск строки с максимальным количеством положительных элементов
         int maxPositiveCountRow = FindRowWithMaxPositiveCount(B);
- 
+
         // Поиск столбца с максимальным количеством положительных элементов
         int maxPositiveCountColumn = FindColumnWithMaxPositiveCount(C);
- 
+
         // Вставка столбца C после строки с максимальным количеством положительных элементов
         int[,] result = InsertColumnAfterRow(B, C, maxPositiveCountRow, maxPositiveCountColumn);
- 
+
         // Вывод результата
         Console.WriteLine("Вывод результата: ");
         PrintArray(result);
+
+        int[] onedimensionalArray = ConvertToOnedimensionalArray(result);
+        SortArray(onedimensionalArray);
+
+        Console.WriteLine("Отсортированный одномерный массив:");
+        foreach (int element in onedimensionalArray)
+        {
+            Console.Write($"{element} ");
+        }
         Console.ReadKey();
-    }
- 
+}
     static int FindRowWithMaxPositiveCount(int[,] array)
     {
         int maxPositiveCount = 0;
         int maxPositiveCountRow = -1;
- 
+
         for (int i = 0; i < array.GetLength(0); i++)
         {
             int positiveCount = 0;
- 
+
             for (int j = 0; j < array.GetLength(1); j++)
             {
                 if (array[i, j] > 0)
@@ -91,26 +98,26 @@ class Program
                     positiveCount++;
                 }
             }
- 
+
             if (positiveCount > maxPositiveCount)
             {
                 maxPositiveCount = positiveCount;
                 maxPositiveCountRow = i;
             }
         }
- 
+
         return maxPositiveCountRow;
     }
- 
+
     static int FindColumnWithMaxPositiveCount(int[,] array)
     {
         int maxPositiveCount = 0;
         int maxPositiveCountColumn = -1;
- 
+
         for (int j = 0; j < array.GetLength(1); j++)
         {
             int positiveCount = 0;
- 
+
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 if (array[i, j] > 0)
@@ -118,26 +125,26 @@ class Program
                     positiveCount++;
                 }
             }
- 
+
             if (positiveCount > maxPositiveCount)
             {
                 maxPositiveCount = positiveCount;
                 maxPositiveCountColumn = j;
             }
         }
- 
+
         return maxPositiveCountColumn;
     }
- 
+
     static int[,] InsertColumnAfterRow(int[,] arrayB, int[,] arrayC, int row, int column)
     {
         int rowsB = arrayB.GetLength(0);
         int columnsB = arrayB.GetLength(1);
         int rowsC = arrayC.GetLength(0);
         int columnsC = arrayC.GetLength(1);
- 
+
         int[,] result = new int[rowsB + 1, columnsB];
- 
+
         // Копирование массива B до строки с максимальным количеством положительных элементов
         for (int i = 0; i <= row; i++)
         {
@@ -146,16 +153,16 @@ class Program
                 result[i, j] = arrayB[i, j];
             }
         }
- 
+
         // Вставка столбца C после строки с максимальным количеством положительных элементов
         for (int i = 0, j = 0; j < columnsB; i++, j++)
         {
             //for (int j = 0; j < columnsC; j++)
             //{
-                result[row + 1, j] = arrayC[i, column];
+            result[row + 1, j] = arrayC[i, column];
             //}
         }
- 
+
         // Копирование оставшейся части массива B
         for (int i = row + 2; i <= rowsB; i++)
         {
@@ -164,10 +171,10 @@ class Program
                 result[i, j] = arrayB[i - 1, j];
             }
         }
- 
+
         return result;
     }
- 
+
     static void PrintArray(int[,] array)
     {
         for (int i = 0; i < array.GetLength(0); i++)
@@ -178,6 +185,93 @@ class Program
             }
             Console.WriteLine();
         }
+   }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    static int[] ConvertToOnedimensionalArray(int[,] array)
+    {
+        int[] onedimensionalArray = new int[array.GetLength(0) * array.GetLength(1)];
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                onedimensionalArray[i * array.GetLength(1) + j] = array[i, j];
+            }
+        }
+
+        return onedimensionalArray;
+    }
+
+    static void SortArray(int[] array)
+    {
+        MergeSort(array, 0, array.Length - 1);
+    }
+
+    static void MergeSort(int[] array, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+
+            MergeSort(array, left, middle);
+            MergeSort(array, middle + 1, right);
+
+            Merge(array, left, middle, right);
+        }
+    }
+
+    static void Merge(int[] array, int left, int middle, int right)
+    {
+        int[] tempArray = new int[right - left + 1];
+
+        int i = left;
+        int j = middle + 1;
+        int k = 0;
+
+        while (i <= middle && j <= right)
+        {
+            if (array[i] <= array[j])
+            {
+                tempArray[k] = array[i];
+                i++;
+            }
+            else
+            {
+                tempArray[k] = array[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i <= middle)
+        {
+            tempArray[k] = array[i];
+            i++;
+            k++;
+        }
+
+        while (j <= right)
+        {
+            tempArray[k] = array[j];
+            j++;
+            k++;
+        }
+
+        for (k = 0; k < tempArray.Length; k++)
+        {
+            array[left + k] = tempArray[k];
+        }
     }
 }
- 
+
