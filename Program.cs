4c7560983fca37f3c38b5b542290df363ad167a2
UshakovaAlexandra <146060562+UshakovaAlexandra@users.noreply.github.com>
2024-03-01 19:33:24 +0300

    переделанная лаба 7

--- Program.cs
+++ Program.cs
@@ -1,172 +1,4 @@
 // 7 лаба.
-// 1.1
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class Participant
-{
-    private string lastName;
-    private string society;
-    private double firstAttempt;
-    private double secondAttempt;
-    private bool disqualified;
-
-    public string LastName
-    {
-        get { return lastName; }
-        set { lastName = value; }
-    }
-
-    public string Society
-    {
-        get { return society; }
-        set { society = value; }
-    }
-
-    public double FirstAttempt
-    {
-        get { return firstAttempt; }
-        set { firstAttempt = value; }
-    }
-
-    public double SecondAttempt
-    {
-        get { return secondAttempt; }
-        set { secondAttempt = value; }
-    }
-
-    public bool Disqualified
-    {
-        get { return disqualified; }
-        set { disqualified = value; }
-    }
-
-    public double GetTotalDistance()
-    {
-        return FirstAttempt + SecondAttempt;
-    }
-
-    public void Disqualify()
-    {
-        disqualified = true;
-    }
-
-    public void PrintParticipantInfo()
-    {
-        Console.WriteLine($"{LastName}\t{Society}\t{GetTotalDistance()}");
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        List<Participant> participants = new List<Participant>();
-
-        // Добавление участников в список
-        participants.Add(new Participant { LastName = "Иванов", Society = "Общество 1", FirstAttempt = 7.2, SecondAttempt = 7.5 });
-        participants.Add(new Participant { LastName = "Петров", Society = "Общество 2", FirstAttempt = 7.8, SecondAttempt = 8.0 });
-        participants.Add(new Participant { LastName = "Сидоров", Society = "Общество 1", FirstAttempt = 6.0, SecondAttempt = 6.2 });
-
-        // Дисквалификация участника
-        DisqualifyParticipant(participants, "Сидоров");
-
-        // Сортировка результатов по сумме двух попыток и исключение дисквалифицированных участников
-        participants = participants.Where(p => !p.Disqualified).OrderByDescending(p => p.GetTotalDistance()).ToList();
-
-        // Вывод протокола
-        Console.WriteLine("Место\tФамилия\tОбщество\tРезультат");
-        for (int i = 0; i < participants.Count; i++)
-        {
-            Participant participant = participants[i];
-            participant.PrintParticipantInfo();
-        }
-    }
-
-    static void DisqualifyParticipant(List<Participant> participants, string lastName)
-    {
-        Participant participantToDisqualify = participants.Find(p => p.LastName == lastName);
-        if (participantToDisqualify != null)
-        {
-            participantToDisqualify.Disqualify();
-        }
-    }
-}
-// 1.2
-using System;
-using System.Collections.Generic;
-
-class Person
-{
-    private string fullName;
-
-    public string FullName
-    {
-        get { return fullName; }
-    }
-
-    public Person(string fullName)
-    {
-        this.fullName = fullName;
-    }
-}
-
-class Student : Person
-{
-    public string StudentID { get; set; }
-    public List<double> ExamScores { get; set; }
-
-    public double GetAverageScore()
-    {
-        double sum = 0;
-        foreach (double score in ExamScores)
-        {
-            sum += score;
-        }
-        return sum / ExamScores.Count;
-    }
-
-    public Student(string fullName, string studentID, List<double> examScores) : base(fullName)
-    {
-        StudentID = studentID;
-        ExamScores = examScores;
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        List<Student> students = new List<Student>();
-
-        // Добавление студентов в список
-        students.Add(new Student("Иванов Михаил Романович", "12345", new List<double> { 4.5, 3.8, 3.2, 3.7 }));
-        students.Add(new Student("Сидоров Валерий Иванович", "54321", new List<double> { 4.2, 4.0, 4.5, 3.9 }));
-        students.Add(new Student("Смирнов Александр Сергеевич", "98765", new List<double> { 3.9, 4.1, 4.3, 4.4 }));
-        students.Add(new Student("Николаева Зинаида Петровна", "13579", new List<double> { 3.9, 4.1, 4.3, 4.2 }));
-
-        // Фильтрация студентов по среднему баллу
-        List<Student> filteredStudents = new List<Student>();
-        foreach (Student student in students)
-        {
-            if (student.GetAverageScore() >= 4)
-            {
-                filteredStudents.Add(student);
-            }
-        }
-
-        // Сортировка студентов по убыванию среднего балла
-        filteredStudents.Sort((s1, s2) => s2.GetAverageScore().CompareTo(s1.GetAverageScore()));
-
-        // Вывод результатов в виде таблицы
-        Console.WriteLine("ФИО\t\t\t\tНомер студ. билета\tСредний балл");
-        foreach (Student student in filteredStudents)
-        {
-            Console.WriteLine($"{student.FullName}\t\t{student.StudentID}\t\t\t{student.GetAverageScore()}");
-        }
-    }
-}
 // 6.3
 using System;
 using System.Collections.Generic;
@@ -175,7 +7,13 @@ using System.Linq;
 class Country
 {
     public string CountryName { get; set; }
-    public List<string> Answers { get; set; }
+    public Dictionary<string, int> Answers { get; set; }
+
+    public Country(string countryName, Dictionary<string, int> answers)
+    {
+        CountryName = countryName;
+        Answers = answers;
+    }
 
     public virtual string GetCountryName()
     {
@@ -203,6 +41,8 @@ class Country
 
 class Russia : Country
 {
+    public Russia(Dictionary<string, int> answers) : base("Россия", answers) { }
+
     public override string GetCountryName()
     {
         return "Россия";
@@ -211,6 +51,8 @@ class Russia : Country
 
 class Japan : Country
 {
+    public Japan(Dictionary<string, int> answers) : base("Япония", answers) { }
+
     public override string GetCountryName()
     {
         return "Япония";
@@ -219,31 +61,21 @@ class Japan : Country
 
 class Survey
 {
-    public List<string> AnimalAnswers { get; set; }
-    public List<string> CharacterTraitAnswers { get; set; }
-    public List<string> ConceptAnswers { get; set; }
+    public Dictionary<string, List<string>> Questions { get; set; }
 
-    public Dictionary<string, double> GetTopAnswers(List<string> answers)
+    public Survey(Dictionary<string, List<string>> questions)
+    {
+        Questions = questions;
+    }
+
+    public Dictionary<string, double> GetTopAnswers(Dictionary<string, int> answers)
     {
         Dictionary<string, double> topAnswers = new Dictionary<string, double>();
-
+        int totalAnswers = answers.Values.Sum();
         foreach (var answer in answers)
         {
-            if (topAnswers.ContainsKey(answer))
-            {
-                topAnswers[answer]++;
-            }
-            else
-            {
-                topAnswers[answer] = 1;
-            }
+            topAnswers[answer.Key] = (double)answer.Value / totalAnswers * 100;
         }
-
-        foreach (var answer in topAnswers.Keys.ToList())
-        {
-            topAnswers[answer] = (topAnswers[answer] / answers.Count) * 100;
-        }
-
         return topAnswers;
     }
 }
@@ -252,31 +84,47 @@ class Program
 {
     static void Main()
     {
-        Survey survey = new Survey();
-
         // Получение ответов на вопросы
-        survey.AnimalAnswers = new List<string> { "Кошка", "Собака", "Рыба", "Рыба", "Кошка", "Кролик", "Кошка" };
-        survey.CharacterTraitAnswers = new List<string> { "Самообладание", "Вежливость", "Трудолюбие", "Трудолюбие", "Трудолюбие" };
-        survey.ConceptAnswers = new List<string> { "Чайная церемония", "Сакура", "Сакура", "Суши", "Самурай", "Суши", "Сакура" };
+        var surveyQuestions = new Dictionary<string, List<string>>
+        {
+            { "Животные", new List<string> { "Кошка", "Собака", "Рыба", "Рыба", "Кошка", "Кролик", "Кошка" } },
+            { "Черты характера", new List<string> { "Самообладание", "Вежливость", "Трудолюбие", "Трудолюбие", "Трудолюбие" } },
+            { "Понятия", new List<string> { "Чайная церемония", "Сакура", "Сакура", "Суши", "Самурай", "Суши", "Сакура" } }
+        };
+        var survey = new Survey(surveyQuestions);
 
         // Создание объектов для стран
-        Country russia = new Russia { CountryName = "Россия", Answers = survey.AnimalAnswers };
-        Country japan = new Japan { CountryName = "Япония", Answers = survey.ConceptAnswers };
+        var russiaAnswers = new Dictionary<string, int> { { "Кошка", 3 }, { "Рыба", 2 }, { "Кролик", 1 } };
+        var russia = new Russia(russiaAnswers);
+
+        var japanAnswers = new Dictionary<string, int> { { "Сакура", 4 }, { "Суши", 2 }, { "Самурай", 1 } };
+        var japan = new Japan(japanAnswers);
 
         // Получение ответов для каждой страны
-        Dictionary<string, double> topRussiaAnswers = survey.GetTopAnswers(russia.Answers);
-        Dictionary<string, double> topJapanAnswers = survey.GetTopAnswers(japan.Answers);
+        var topRussiaAnswers = survey.GetTopAnswers(russia.Answers);
+        var topJapanAnswers = survey.GetTopAnswers(japan.Answers);
 
         // Вывод результатов для каждой страны
         russia.PrintSurveyResults(topRussiaAnswers);
         japan.PrintSurveyResults(topJapanAnswers);
 
         // Объединенные ответы
-        List<string> combinedAnswers = new List<string>();
-        combinedAnswers.AddRange(russia.Answers);
-        combinedAnswers.AddRange(japan.Answers);
-
-        Dictionary<string, double> topCombinedAnswers = survey.GetTopAnswers(combinedAnswers);
+        var combinedAnswers = new Dictionary<string, int>();
+        foreach (var question in surveyQuestions)
+        {
+            foreach (var answer in question.Value)
+            {
+                if (combinedAnswers.ContainsKey(answer))
+                {
+                    combinedAnswers[answer]++;
+                }
+                else
+                {
+                    combinedAnswers[answer] = 1;
+                }
+            }
+        }
+        var topCombinedAnswers = survey.GetTopAnswers(combinedAnswers);
 
         // Вывод общих результатов
         Console.WriteLine("Общие ответы для обеих стран:");
